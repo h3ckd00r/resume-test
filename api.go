458b799ce82eb1dfb5d5a958b2262ae9df443ba2
shubh <bshubh27@gmail.com>
2023-01-31 01:58:11 +0530

    api.go modded to allow test

--- api.go
+++ api.go
@@ -9,44 +9,48 @@ import (
 	"cloud.google.com/go/firestore"
 )
 
-func main() {
-	//new Firestore client
-	ctx := context.Background()
-	client, err := firestore.NewClient(ctx, "project-resume-24")
-	if err != nil {
-		log.Fatalf("Failed to create Firestore client: %v", err)
-	}
-
-	// API HANDLER
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//allow cors
+// APIHandler
+func APIHandler(client *firestore.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//ALLOWING CORS
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-		// get current value
-		doc, err := client.Collection("counters").Doc("counter").Get(ctx)
+		//GETTING CURRENT VALUE
+		doc, err := client.Collection("counters").Doc("counter").Get(context.Background())
 		if err != nil {
 			log.Fatalf("Failed to get document: %v", err)
 		}
 
 		value := doc.Data()["value"].(int64)
 
-		// incrementing value by 1
+		//VALUE +1
 		value++
 
-		// update value ==> new value
-		_, err = client.Collection("counters").Doc("counter").Set(ctx, map[string]interface{}{
+		//UPDATING OLD VALUE TO NEW VALUE
+		_, err = client.Collection("counters").Doc("counter").Set(context.Background(), map[string]interface{}{
 			"value": value,
 		}, firestore.MergeAll)
 		if err != nil {
 			log.Fatalf("Failed to update document: %v", err)
 		}
-		//writing new value as JSON to response
+		//NEW VALUE AS JSON TO RESP.
 		fmt.Fprintf(w, `{"value": %d}`, value)
-	})
+	}
+}
 
-	//API SERVER ON
+func main() {
+	//FIRESTORE CLIENT
+	client, err := firestore.NewClient(context.Background(), "project-resume-24")
+	if err != nil {
+		log.Fatalf("Failed to create Firestore client: %v", err)
+	}
+
+	//HANDLER
+	http.HandleFunc("/", APIHandler(client))
+
+	//SERVER ON
 	port := "8080"
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
